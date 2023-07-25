a7c86a76e6a586c4ff2f73eae9d7a558b07ae78d
RuwanT <ruwant.email@gmail.com>
2023-07-25 15:06:06 +1000

    Update setup.sh

--- setup-stuff/setup.sh
+++ setup-stuff/setup.sh
@@ -1,17 +1,17 @@
 sudo apt-get update
 
 #install minecraft tools
-mkdir /workspaces/COSC2804_assignment3/temp-stuff
-unzip /workspaces/COSC2804_assignment3/setup-stuff/MinecraftToolsLinux.zip -d /workspaces/COSC2804_assignment3/temp-stuff/MAPI
+mkdir ../temp-stuff
+unzip ../setup-stuff/MinecraftToolsLinux.zip -d ../temp-stuff/MAPI
 pip install --upgrade pip
 
-cd /workspaces/COSC2804_assignment3/temp-stuff/MAPI/Minecraft_Tools/
+cd ../temp-stuff/MAPI/Minecraft_Tools/
 sudo chmod 777 Install_API.sh
 sudo chmod 777 start_server.sh 
 sudo ./Install_API.sh
 
 #install MCPP
-cd /workspaces/COSC2804_assignment3/temp-stuff/
+cd ../../
 git clone https://github.com/rozukke/mcpp.git
 cd mcpp
 cmake .
